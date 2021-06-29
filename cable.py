9d1c9ad735bc04c867cd8d35c0f0ac6f76a27107
srirag-vuppala <srirag.vuppala@gmail.com>
2021-06-28 23:53:01 -0700

    Clean up the code and added comments

--- cable.py
+++ cable.py
@@ -1,99 +1,98 @@
+# imports
 import numpy as np
-import hh as hh
 import math
 import matplotlib.pyplot as plt
 import os 
+import hh 
 
-
-np.set_printoptions(precision=2)
-
-T = 1.5
-N = 1000
-dt = float(T)/float(N)
-
-t = np.array([n*dt for n in range(N)])
-
-""" Cell length , in cm"""
-Cell_len = 0.01
-""" Cell radius is width/2 here width = Cell length/6 (assumption) , in cm """
-Cell_rad = .00241/2#Cell_len/12
-""" Cell perimeter = perimeter of circle , in cm""" 
-Cell_peri = 2*math.pi*Cell_rad
-   
-n_cells = 10 # number of nodes per cell
-J = n_cells*10 # total number of nodes
-dx = float(Cell_len*n_cells)/float(J)
-x_grid = np.array([j*dx for j in range(J)])
 
 def main():
     """ An implementation of the CableEquation where the Hodgkin Huxley equations
     describe the nature of Ionic current """ 
 
-    """Adding in the constants for the equation """
-    """membrane capacitance, in uF/cm^2"""
-    C_m = 1
-    """ resistance per unit len intracellular space, in omega-cm  """
-    r_i = 1/(math.pi*Cell_rad*Cell_rad*6.7)
-    """ resistance per unit len extracellular space, in omega-cm """
-    r_e = 1/(math.pi*(Cell_rad+0.00001)*(Cell_rad+0.00001)*20) 
+    """ Defining our constants and assumptions """
+    # Total duration of the simulation (in sec)
+    T = 1.5
+    # The number of time steps that we track for 
+    N = 1000
+    # The time that passes by between each time step 
+    dt = float(T)/float(N)
+
+
+    # Cell length (in cm)
+    Cell_len = 0.01
+    # Cell radius is width/2 (in cm)
+    # width = Cell length/6 [assumption: the cell would be roughly 6:1 | length:width] 
+    Cell_rad = .00241/2 
+    # Cell perimeter = perimeter of circle (in cm)
+    Cell_peri = 2*math.pi*Cell_rad
     
+    # A node is point in space where we observe the action potential
+    n_cells = 10 # number of nodes per cell
+    J = n_cells*10 # total number of nodes
+    # The seperation between two nodes (in cm)
+    dx = float(Cell_len*n_cells)/float(J)
+    # Every node that we observe in an array
+    x_grid = np.array([j*dx for j in range(J)])
+
+
+    # membrane capacitance, (in uF/cm^2)
+    C_m = 1
+
+    # resistance per unit len intracellular space, (in omega-cm)
+    # r_i = 1/(math.pi*Cell_rad*Cell_rad*6.7)
+    # resistance per unit len extracellular space, (in omega-cm) 
+    # r_e = 1/(math.pi*(Cell_rad+0.00001)*(Cell_rad+0.00001)*20) 
+    
+    # We use the constant values below for the simulation but they are governed as shown above
     r_i = 178330.0
     r_e = 888740.0
 
-    #resting potential = -70
+    # A array of transmembrane potentials that exists at each node (in mV)
+    # resting potential = -70
     V_old= [-70 for j in range(J)]
 
-    """ This is the D*delta(t)/delta(x)^2 """
-    #D_v = dt/(2*C_m*Cell_peri*(r_i + r_e)*dx*dx)
+    # Laplacian
+    # The D_v is the constant that gets multiplied to the laplacian matrix
     D_v = dt/((r_i + r_e)*dx*dx*Cell_peri)
     
-    A_v = np.diagflat([-D_v/2 for i in range(J-1)], -1) + np.diagflat([1. + D_v for i in range(J)]) + np.diagflat([-1*D_v/2 for i in range(J-1)], 1)
+    # Constructing the Laplacian for the (n+1)th time step
+    A_v = np.diagflat([-D_v/2 for _ in range(J-1)], -1) + np.diagflat([1. + D_v for _ in range(J)]) + np.diagflat([-1*D_v/2 for _ in range(J-1)], 1)
+    # Defining the boundary conditions
+    # Note there are only two boundaries for the cable equation because there are only two ends for a cable
     A_v[0][0] = 1+D_v/2
     A_v[-1][-1] = 1+D_v/2
 
-    B_v = np.diagflat([ D_v/2 for i in range(J-1)], -1) + np.diagflat([1. - D_v for i in range(J)]) + np.diagflat([D_v/2 for i in range(J-1)], 1)
+    # Constructing the Laplacian for the (n)th time step
+    B_v = np.diagflat([ D_v/2 for _ in range(J-1)], -1) + np.diagflat([1. - D_v for _ in range(J)]) + np.diagflat([D_v/2 for _ in range(J-1)], 1)
+    # Defining the boundary conditions
     B_v[0][0] = 1-D_v/2
     B_v[-1][-1] = 1-D_v/2
 
-    print(A_v)
-    print(B_v)
-   
     #change this value for the actual nth printing of the graph
     nplot = 10
-    c=0
+    c = 0
 
+    # injected current
     val = 30
-    V_old[0] = -70+val 
-    V_old[1] = -70+val 
-    V_old[2] = -70+val 
-    V_old[-1] =  -70-val 
-    V_old[-2] =  -70-val 
-    V_old[-3] =  -70-val 
-
-    dn = hh.HodgkinHuxley().n_inf([j+70.0 for j in V_old])
-    dm =hh.HodgkinHuxley().m_inf([j+70.0 for j in V_old])
-    dh = hh.HodgkinHuxley().h_inf([j+70.0 for j in V_old])
-
-    print(V_old)
-   
     # Actually making
-    for i in range(1,N):
+    # for i in range(1,N):
+    for i in range(N):
+        # Continually inject current for the first 200 timesteps
+        # This will serve as our excitation that we artificially give to spark the propagation of the action potential
+        if i < 200:
+            V_old[0]  = -70+val 
+            V_old[1]  = -70+val 
+            V_old[2]  = -70+val 
+            V_old[-1] = -70-val 
+            V_old[-2] = -70-val 
+            V_old[-3] = -70-val
         # Here i is the ith time step in the entire simulation
-                    
-        hh_arr= hh.HodgkinHuxley().main([j+70.0 for j in V_old], dn, dm , dh)
-        I_ion_old = hh_arr[3]
-        
+        I_ion_old = hh.HodgkinHuxley().main([j+70.0 for j in V_old])
         B_part = np.matmul(B_v, V_old) - dt*np.asarray(I_ion_old)
         V_new = np.linalg.solve(A_v, B_part)
         
-        # turn off I ion 
-        #I_ion_old = [0]*J
-        dn = hh_arr[0]
-        dm = hh_arr[1]
-        dh = hh_arr[2]
-        
-        
-        if(i%nplot==0): #plot results every nplot timesteps
+        if(i % nplot == 0): #plot results every nplot timesteps
             plt.plot(x_grid,V_new,linewidth=2)
             plt.ylim([-100, 100])
             #plt.xlim([-2,35])
@@ -108,14 +107,8 @@ def main():
             c += 1
         
         V_old = V_new
-        if i < 200:
-            V_old[0] = -70+val 
-            V_old[1] = -70+val 
-            V_old[2] = -70+val 
-            V_old[-1] =  -70-val 
-            V_old[-2] =  -70-val 
-            V_old[-3] =  -70-val 
-        
+         
+# snapshots the operating system takes of the plotted graphs to make it into a video
 os.system("ffmpeg -y -i 'foo%03d.jpg' cable_eqn.m4v")
 os.system("rm -f *.jpg")
 
--- hh.py
+++ hh.py
@@ -1,89 +1,64 @@
 import numpy as np
-import cable
 
 class HodgkinHuxley():
+    # membrane capacitance, in uF/cm^2
     C_m = 1.0
-    """membrane capacitance, in uF/cm^2"""
+    # Sodium (Na) maximum conductances, in mS/cm^2
     g_Na = 120.0
-    """Sodium (Na) maximum conductances, in mS/cm^2"""
+    # Postassium (K) maximum conductances, in mS/cm^2
     g_K = 36.0
-    """Postassium (K) maximum conductances, in mS/cm^2"""
+    # Leak maximum conductances, in mS/cm^2
     g_L = 0.3
-    """Leak maximum conductances, in mS/cm^2"""
+    # Sodium (Na) Diffusion potentials, in mV
     V_Na = 115.0
-    """Sodium (Na) Diffusion potentials, in mV"""
+    # Postassium (K) Diffusion potentials, in mV
     V_K  = -12.0
-    """Postassium (K) Diffusion potentials, in mV"""
+    # Leak current Diffusion potentials, in mV
     V_L  = 10.6
-    """Leak current Diffusion potentials, in mV"""
+
     counter = 0 
 
     def alpha_m(self, V):
-        """Channel gating kinetics. Functions of membrane voltage"""
-        # THis is 25 because apparently the exp fucntion doesn't like np.exp(0)
-        if V == 25:
-            V = 24.99
-        #print(0.1*(25.0 - V)/(np.exp(((25.0-V) / 10.0)) - 1.0))
+        # Channel gating kinetics. Functions of membrane voltage
         return 0.1*(25.0 - V)/(np.exp(2.5 - (0.1 * V)) - 1.0)
     def beta_m(self, V):
-        """Channel gating kinetics. Functions of membrane voltage"""
+        # Channel gating kinetics. Functions of membrane voltage
         return 4.0*np.exp(-(V / 18.0))
 
     def alpha_h(self, V):
-        """Channel gating kinetics. Functions of membrane voltage"""
+        # Channel gating kinetics. Functions of membrane voltage
         return 0.07*np.exp(-(V / 20.0))
     def beta_h(self, V):
-        """Channel gating kinetics. Functions of membrane voltage"""
+        # Channel gating kinetics. Functions of membrane voltage
         return 1.0/(1.0 + np.exp(3.0 - (0.1* V)))
 
     def alpha_n(self, V):
-        """Channel gating kinetics. Functions of membrane voltage"""
-        # THis is 25 because apparently the exp fucntion doesn't like np.exp(0)
-        if V == 10:
-            V=9.99
+        # Channel gating kinetics. Functions of membrane voltage"""
         return (0.01*(10.0 - V))/(np.exp(1.0 - (0.1*V)) - 1.0)
     def beta_n(self, V):
-        """Channel gating kinetics. Functions of membrane voltage"""
+        # Channel gating kinetics. Functions of membrane voltage
         return 0.125*np.exp(-(V / 80.0))
     
-    # Add round here as well
     def n_inf(self, Vm):
-        """ Inflection point potassium conductance to easily write gK"""
+        # Inflection point potassium conductance to easily write gK
         final = []
         for v in Vm:
-            #print(v)
             final.append(self.alpha_n(v) / (self.alpha_n(v) + self.beta_n(v)))
         return final
     def m_inf(self, Vm):
-        """ Sodium activation variable """
+        # Sodium activation variable 
         final = []
         for i in Vm:
             final.append(self.alpha_m(i) / (self.alpha_m(i) + self.beta_m(i)))
         return final
     def h_inf(self, Vm):
-        """ Sodium inactivation variable """
+        # Sodium inactivation variable 
         final = []
         for i in Vm:
             final.append(self.alpha_h(i) / (self.alpha_h(i) + self.beta_h(i)))
         return final
 
-    def main(self, V_old, dn, dm ,dh):
-#        n = []
-#        m = []
-#        h = []
-#        # dn = self.n_inf(V_old)
-#        # dm = self.m_inf(V_old)
-#        # dh = self.h_inf(V_old)
-#        fin_n = []
-#        fin_h = []
-#        fin_m = []
-#        for i in range(len(V_old)):
-#            fin_n.append(min(dn[i]+cable.dt*(self.alpha_n(V_old[i])*(1-dn[i]) - self.beta_n(V_old[i])), 1.0))
-#            fin_m.append(min(dm[i]+cable.dt*(self.alpha_m(V_old[i])*(1-dn[i]) - self.beta_m(V_old[i])), 1.0))
-#            fin_h.append(min(dh[i]+cable.dt*(self.alpha_h(V_old[i])*(1-dn[i]) - self.beta_h(V_old[i])) ,1.0))
-#        n = fin_n 
-#        m = fin_m 
-#        h = fin_h 
+    def main(self, V_old):
         n=self.n_inf(V_old)
         m=self.m_inf(V_old)
         h=self.h_inf(V_old)
@@ -92,7 +67,6 @@ class HodgkinHuxley():
         GNa = []
         GL = []
         for i in range(len(V_old)):
-            
             GK.append((self.g_K / self.C_m) * np.power(n[i], 4.0))
             #GNa.append((self.g_Na / self.C_m) * np.power(m[i], 3.0) * h[i])
             GNa.append((self.g_Na / self.C_m) * np.power(m[i], 3.0) * 0.596)
@@ -101,7 +75,7 @@ class HodgkinHuxley():
         for i in range(len(V_old)):
             I.append((GK[i] * (V_old[i] - self.V_K)) + (GNa[i] * (V_old[i] - self.V_Na)) + (GL[i] * (V_old[i] - self.V_L)) - 0) 
 
-        return [n, m, h, I]
+        return I
 
 if __name__ == '__main__':
     HodgkinHuxley().main()
